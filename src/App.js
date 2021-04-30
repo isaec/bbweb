8d0d11ee731c67acf0d68add1f3d0cc2b01b6cd4
isaac <isaac@rubenfamily.com>
2021-04-30 15:46:49 -0700

    basic connection stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 
+let channel = null
+const connection = new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] })
+
+connection.ondatachannel = event => {
+  console.log("ondatachannel")
+  channel = event.channel
+  channel.onopen = console.log
+  channel.onmessage = console.log
+}
+
+console.log(connection)
+
 const Avatar = () =>
   <svg viewBox="0 0 100 100" className="Avatar">
     <circle cx="50" cy="50" r="50" fill="pink" />
   </svg>
-
-
 
 const Message = props =>
   <div className="message">
@@ -17,10 +27,28 @@ const Message = props =>
     </div>
   </div>
 
+class ConnectionState extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      con: connection.connectionState,
+      ice: connection.iceConnectionState
+    }
+    connection.onconnectionstatechange = () => this.setState({con: connection.connectionState})
+    connection.oniceconnectionstatechange = () => this.setState({ice: connection.iceConnectionState})
+  }
+  render() {
+    return <div className="ConnectionState">
+      <p>connection state: {this.state.con}</p>
+      <p>ice state: {this.state.ice}</p>
+    </div>
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <ConnectionState />
       <Message name="bob" content="my message is long, but you shall hear it" />
       <Message name="bob" content="even if you do not like it" />
       <Message name="mark" content="really?" />
